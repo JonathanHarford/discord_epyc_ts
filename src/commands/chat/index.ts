86284558529c2dbdffaf1e94f70ae92741f3b0b3
Jonathan Harford <jonathan.harford@gmail.com>
2025-05-13 16:22:52 -0700

    undo all join season stuff

--- src/commands/chat/index.ts
+++ src/commands/chat/index.ts
@@ -3,4 +3,4 @@ export { HelpCommand } from './help-command.js';
 export { InfoCommand } from './info-command.js';
 export { TestCommand } from './test-command.js';
 export { command as NewCommand } from './new-command.js';
-export { JoinSeasonCommand } from './join-season-command.js';
+// export { JoinSeasonCommand } from './join-season-command.js';
